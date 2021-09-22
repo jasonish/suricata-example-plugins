9f0183778d5ebc371bf6f11e4bd213a74f44fc20
Jason Ish <ish@unx.ca>
2021-09-22 14:55:06 -0600

    eve-filetype: cleanup and more doc

--- eve-filetype/README.md
+++ eve-filetype/README.md
@@ -1,14 +1,14 @@
 # EVE plugin for Suricata 7.0.x
 
-This is an example EVE plugin for Suricata.
+This is an example EVE output plugin for Suricata 7.0 (git master).
 
 ## Building
 
-To build this plugin you will first need to have the Suricata
-development headers and libraries installed. At the time of this
-writing this means building Suricata from source and running:
-- `make install-headers`
+Before building this plugin you will need to build and install Suricata from the
+git master branch and install the development tools and headers:
+
 - `make install-library`
+- `make install-headers`
 
 then make sure the newly installed tool `libsuricata-config` can be
 found in your path, for example:
@@ -18,13 +18,20 @@ libsuricata-config --cflags
 
 Then a simple `make` should build this plugin.
 
+Or if the Suricata installation is not in the path, a command like the following
+can be used:
+
+```
+PATH=/opt/suricata/bin:$PATH make
+```
+
 ## Running
 
 To run the plugin, first add the path to the plugin you just compiled to
 your `suricata.yaml`, for example:
 ```
 plugins:
-  - /home/jason/oisf/code/suricata-example-plugins/eve-file-type/eve-filetype.so
+  - /home/jason/src/suricata-example-plugins/eve-filetype/eve-filetype.so
 ```
 
 Then add an output for the plugin:
@@ -32,13 +39,65 @@ Then add an output for the plugin:
 outputs:
   - eve-log:
       enabled: yes
-      filetype: template-filetype-plugin
+      filetype: eve-filetype
+      threaded: true
       types:
         - dns
         - tls
         - http
 ```
 
-In the example above we use the name specified in the plugin as the
-`filetype` and specify that all `dns`, `tls` and `http` log entries
-should be sent to the plugin.
+In the example above we use the name specified in the plugin as the `filetype`
+and specify that all `dns`, `tls` and `http` log entries should be sent to the
+plugin.
+
+## Details
+
+This plugin demonstrates a Suricata EVE output plugin (file-type). The idea of a
+Suricata EVE output plugin is to provide a file like interface for the handling
+of rendered EVE (JSON) logs. This is useful for custom destinations not builtin
+to Suricata or if the formatted JSON requires some post-processing.
+
+Note: EVE output plugins are not that useful just for reformatting the JSON
+output as the plugin does need to handling writing to a file once the file type
+has been delegated to the plugin.
+
+### Registering a Plugin
+
+All Suricata plugins make themselves known to Suricata in the way using a
+function named `SCPluginRegister` which is called after Suricata loads the
+plugin shared object file. This function must return a `SCPlugin` struct which
+contains basic information about the plugin.  For example:
+
+```c
+const SCPlugin PluginRegistration = {
+    .name = "eve-filetype",
+    .author = "Jason Ish",
+    .license = "GPLv2",
+    .Init = TemplateInit,
+};
+
+const SCPlugin *SCPluginRegister() {
+    return &PluginRegistration;
+}
+```
+
+### Initializing a Plugin
+
+After the plugin has been registered, the `Init` callback will be called. This
+is where the plugin will set itself up as a specific type of plugin such as an
+EVE output, or a capture method.
+
+This plugins registers itself as an EVE file type using the `SCPluginFileType`
+struct. To register as an EVE file type the following must be provided:
+
+* name: This is the name of the output which will be used in the eve filetype
+  field in `suricata.yaml` to enable this output.
+* Init: The callback called when the output is "opened".
+* Deinit: The callback called the output is "closed".
+* ThreadInit: Callback called to initialize per thread data (if threaded).
+* ThreadDeinit: Callback called to deinitialize per thread data (if threaded).
+* Write: The callback called when an EVE record is to be "written".
+* Close: The callback called when output is to be "closed".
+
+Please see the code in `template.c` for more details about this functions.
--- eve-filetype/template.c
+++ eve-filetype/template.c
@@ -22,24 +22,75 @@
 #include "util-mem.h"
 #include "util-debug.h"
 
-#define OUTPUT_NAME "template-filetype-plugin"
+#define OUTPUT_NAME "eve-filetype"
 
-typedef struct Context_ {
-} Context;
+static int ThreadInit(void *ctx, int thread_id, void **thread_data);
+static int ThreadDeinit(void *ctx, void *thread_data);
 
+/**
+ * Per thread context data for each logging thread.
+ */
 typedef struct ThreadData_ {
+    /** The thread ID, for demonstration purposes only. */
     int thread_id;
+
+    /** The number of records logged on this thread. */
+    uint64_t count;
 } ThreadData;
 
+/**
+ * A context object for each eve logger using this output.
+ */
+typedef struct Context_ {
+    /** Verbose, or print to stdout. */
+    int verbose;
+
+    /** A thread context to use when not running in threaded mode. */
+    ThreadData *thread;
+} Context;
+
+/**
+ * This function is called when the output is closed.
+ *
+ * This will be called after ThreadDeinit is called for each thread.
+ *
+ * \param data The data allocated in TemplateOpen. It should be cleaned up and
+ *      deallocated here.
+ */
 static void TemplateClose(void *data)
 {
     printf("TemplateClose\n");
     Context *ctx = data;
     if (ctx != NULL) {
+        if (ctx->thread) {
+            ThreadDeinit(ctx, (void *)ctx->thread);
+        }
         SCFree(ctx);
     }
 }
 
+/**
+ * This function is called to initialize the output, it can be somewhat thought
+ * of like opening a file.
+ *
+ * \param conf The EVE configuration node using this output.
+ *
+ * \param threaded If true the EVE subsystem is running in threaded mode.
+ *
+ * \param data A pointer where context data can be stored relevant to this
+ *      output.
+ *
+ * Eve output plugins need to be thread aware as the threading happens at lower
+ * level than the EVE output, so a flag is provided here to notify the plugin if
+ * threading is enabled or not.
+ *
+ * If the plugin does not work with threads disabled, or enabled, this function
+ * should return -1.
+ *
+ * Note for upgrading a plugin from 6.0 to 7.0: The ConfNode in 7.0 is the
+ * configuration for the eve instance, not just a node named after the plugin.
+ * This allows the plugin to get more context about what it is logging.
+ */
 static int TemplateInitOutput(ConfNode *conf, bool threaded, void **data)
 {
     SCLogNotice("Initializing template eve output plugin: threaded=%d", threaded);
@@ -47,16 +98,50 @@ static int TemplateInitOutput(ConfNode *conf, bool threaded, void **data)
     if (context == NULL) {
         return -1;
     }
+
+    /* Verbose by default. */
+    int verbose = 1;
+
+    if (conf && (conf = ConfNodeLookupChild(conf, "eve-template")) != NULL) {
+        if (!ConfGetChildValueBool(conf, "verbose", &verbose)) {
+            verbose = 1;
+        } else {
+            SCLogNotice("Read verbose configuration value of %d", verbose);
+        }
+    }
+    context->verbose = verbose;
+
+    if (!threaded) {
+        /* We're not running in threaded mode so allocate a thread context here
+         * to avoid duplication of context data such as file pointers, database
+         * connections, etc. */
+        if (ThreadInit(context, 0, (void **)&context->thread) != 0) {
+            SCFree(context);
+            return -1;
+        }
+    }
     *data = context;
     return 0;
 }
 
 /**
- * If in threaded mode this method will be called once for each logging thread
- * for thread specific initialization. In regular file output this is where a
- * file would be opened that is prefixed with the thread id. In a plugin this
- * could be a database connection or something (but note that we don't think
- * logging directly to a database is a good idea).
+ * Initialize per thread context.
+ *
+ * \param ctx The context created in TemplateInitOutput.
+ *
+ * \param thread_id An identifier for this thread.
+ *
+ * \param thread_data Pointer where thread specific context can be stored.
+ *
+ * When the EVE output is running in threaded mode this will be called once for
+ * each output thread with a unique thread_id. For regular file logging in
+ * threaded mode Suricata uses the thread_id to construct the files in the form
+ * of "eve.<thread_id>.json". This plugin may want to do similar, or open
+ * multiple connections to whatever the final logging location might be.
+ *
+ * In the case of non-threaded EVE logging this function is NOT called by
+ * Suricata, but instead this plugin chooses to use this method to create a
+ * default (single) thread context.
  */
 static int ThreadInit(void *ctx, int thread_id, void **thread_data)
 {
@@ -71,6 +156,12 @@ static int ThreadInit(void *ctx, int thread_id, void **thread_data)
     return 0;
 }
 
+/**
+ * Deinitialize a thread.
+ *
+ * This is where any cleanup per thread should be done including free'ing of the
+ * thread_data if needed.
+ */
 static int ThreadDeinit(void *ctx, void *thread_data)
 {
     if (thread_data == NULL) {
@@ -79,22 +170,39 @@ static int ThreadDeinit(void *ctx, void *thread_data)
     }
 
     ThreadData *tdata = thread_data;
-    SCLogNotice("Deinitializing thread %d", tdata->thread_id);
+    SCLogNotice(
+            "Deinitializing thread %d: records written: %" PRIu64, tdata->thread_id, tdata->count);
     SCFree(tdata);
     return 0;
 }
 
 /**
  * This method is called with formatted Eve JSON data.
- * 
- * \param buffer Formatted JSON buffer
- * \param buffer_len Length of formatted JSON buffer
- * \param data Data set in Init callback
- * \param thread_data Data set in ThreadInit callbacl
+ *
+ * \param buffer Formatted JSON buffer \param buffer_len Length of formatted
+ * JSON buffer \param data Data set in Init callback \param thread_data Data set
+ * in ThreadInit callbacl
+ *
+ * Do not block in this thread, it will cause packet loss. Instead of outputting
+ * to any resource that may block it might be best to enqueue the buffers for
+ * further processing which will require copying of the provided buffer.
  */
 static int TemplateWrite(const char *buffer, int buffer_len, void *data, void *thread_data)
 {
-    SCLogNotice("Received write with thread_data %p: %s", thread_data, buffer);
+    Context *ctx = data;
+    ThreadData *thread = thread_data;
+
+    /* The thread_data could be null which is valid, or it could be that we are
+     * in single threaded mode. */
+    if (thread == NULL) {
+        thread = ctx->thread;
+    }
+
+    thread->count++;
+
+    if (ctx->verbose) {
+        SCLogNotice("Received write with thread_data %p: %s", thread_data, buffer);
+    }
     return 0;
 }
 
